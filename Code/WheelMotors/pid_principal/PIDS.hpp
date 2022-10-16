9bb07a1a3e172219c70640ca47350862edf89210
alanpelayoo <aln.10zepeda@gmail.com>
2022-10-16 15:17:36 -0500

    modularizing

---
+++ Code/WheelMotors/pid_principal/PIDS.hpp
@@ -0,0 +1,65 @@
+float pv;
+float pv_2;
+
+
+float cv; //Normalmente uk 
+float cv1;
+float error;
+float error1;
+float error2;
+
+float cv_2; //Normalmente uk 
+float cv1_2;
+float error_2;
+float error1_2;
+float error2_2;
+
+float Kp = 2;
+float Ki =10;
+float Kd =0.001;
+float Tm = 0.1; // interval1/ 1000
+
+void pid(int contador, float sp, int enableA){
+  pv = 10*contador*(60.0/374.0); //Rpm
+  error = sp - pv;
+  cv = cv1 + (Kp + Kd/Tm)*error + (-Kp + Ki*Tm - 2*Kd/Tm)*error1 + (Kd/Tm)*error2;
+  cv1 = cv;
+  error2 = error1;
+  error1 = error;
+
+  if (cv > 500.0){
+    cv = 500.0;
+  }
+  if (cv < 30.0){
+    cv = 30.0;
+  }
+  analogWrite(enableA, cv*(255.0/500.0));
+}
+
+void pid2(int contador2, float sp, int enableB){
+  pv_2 = 10*contador2*(60.0/374.0); //Rpm
+  error_2 = sp - pv_2;
+  cv_2 = cv1_2 + (Kp + Kd/Tm)*error_2 + (-Kp + Ki*Tm - 2*Kd/Tm)*error1_2 + (Kd/Tm)*error2_2;
+  cv1_2 = cv_2;
+  error2_2 = error1_2;
+  error1_2 = error_2;
+
+  if (cv_2 > 500.0){
+    cv_2 = 500.0;
+  }
+  if (cv_2 < 30.0){
+    cv_2 = 30.0;
+  }
+  
+  
+  analogWrite(enableB, cv_2*(255.0/500.0));
+}
+
+void print_rpms(float sp){
+  Serial.print("SP: ");
+  Serial.print(sp);
+  Serial.print(", PV: ");
+  Serial.print(pv);
+  Serial.print(", PV2: ");
+  Serial.println(pv_2);
+}
--- Code/WheelMotors/pid_principal/pid_principal.ino
+++ Code/WheelMotors/pid_principal/pid_principal.ino
@@ -1,5 +1,6 @@
 // PID 2 from https://www.youtube.com/watch?v=bl2k6eXDAGM&list=LL&index=1&t=357s
 // PID PRINCIPAL 
+#include "PIDS.hpp"
 
 int enableA = 9;
 int in1 = 8;
@@ -11,38 +12,23 @@ int in3 = 5;
 int in4 = 7;
 int pinB = 3;
 
-float pv;
-float pv_2;
-float sp;
+float sp= 80;
+
 
 volatile int contador = 0;
          int contador2 = 0;
-         
+
+
 unsigned long previousMillis = 0;
 unsigned long previousMillis2 = 0;
 long interval = 100;
 long interval2 = 500;
 
-float cv; //Normalmente uk 
-float cv1;
-float error;
-float error1;
-float error2;
 
-float cv_2; //Normalmente uk 
-float cv1_2;
-float error_2;
-float error1_2;
-float error2_2;
-
-float Kp = 2;
-float Ki =10;
-float Kd =0.001;
-float Tm = 0.1; // interval1/ 1000
 
 void setup() {
   Serial.begin(115200);
-  
+  Serial.println("Hello");
   pinMode(pinA, INPUT);
   pinMode(enableA, OUTPUT);
   pinMode(in1, OUTPUT);
@@ -56,7 +42,6 @@ void setup() {
   attachInterrupt(digitalPinToInterrupt(pinA),interrupcion,RISING);
   attachInterrupt(digitalPinToInterrupt(pinB),interrupcion2,RISING);
   
-  sp = 100;
   analogWrite(enableA, 255);
   digitalWrite(in1, HIGH);
   digitalWrite(in2, LOW);
@@ -69,10 +54,11 @@ void loop() {
   if (( currentMillis - previousMillis) >= interval)
   {
     previousMillis = currentMillis;
-    pid();
-    pid2();
-    analogWrite(enableA, cv*(255.0/500.0));
-    analogWrite(enableB, cv_2*(255.0/500.0));
+    pid(contador,sp,enableA);
+    pid2(contador2,sp,enableB);
+    
+    contador2 = 0;
+    contador = 0;
     
   }
   
@@ -80,12 +66,7 @@ void loop() {
   {
   
     previousMillis2 = currentMillis;
-    Serial.print("SP: ");
-    Serial.print(sp);
-    Serial.print(", PV: ");
-    Serial.print(pv);
-    Serial.print(", PV2: ");
-    Serial.println(pv_2);
+    print_rpms(sp);
   }
 }
 
@@ -95,38 +76,4 @@ void interrupcion() {
 
 void interrupcion2() {
   contador2 ++;
-}
-
-void pid(){
-  pv = 10*contador*(60.0/374.0); //Rpm
-  error = sp - pv;
-  cv = cv1 + (Kp + Kd/Tm)*error + (-Kp + Ki*Tm - 2*Kd/Tm)*error1 + (Kd/Tm)*error2;
-  cv1 = cv;
-  error2 = error1;
-  error1 = error;
-
-  if (cv > 500.0){
-    cv = 500.0;
-  }
-  if (cv < 30.0){
-    cv = 30.0;
-  }
-  contador = 0;
-}
-
-void pid2(){
-  pv_2 = 10*contador2*(60.0/374.0); //Rpm
-  error_2 = sp - pv_2;
-  cv_2 = cv1_2 + (Kp + Kd/Tm)*error_2 + (-Kp + Ki*Tm - 2*Kd/Tm)*error1_2 + (Kd/Tm)*error2_2;
-  cv1_2 = cv_2;
-  error2_2 = error1_2;
-  error1_2 = error_2;
-
-  if (cv_2 > 500.0){
-    cv_2 = 500.0;
-  }
-  if (cv_2 < 30.0){
-    cv_2 = 30.0;
-  }
-  contador2 = 0;
 }
