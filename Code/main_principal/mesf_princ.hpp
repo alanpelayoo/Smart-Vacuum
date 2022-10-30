862c03c331dd6219ccc763dbe60c95ed5cba61dd
alanpelayoo <aln.10zepeda@gmail.com>
2022-10-30 16:10:15 -0600

    save point2

--- Code/main_principal/mesf_princ.hpp
+++ Code/main_principal/mesf_princ.hpp
@@ -33,9 +33,9 @@ void mefRAupdate()
   
   case init_:
     move_forward();
-    us_center = get_cms(usensorC);
-    us_middleL = get_cms(usensormL);
-    us_middleR = get_cms(usensormR);
+    us_center = get_cms(usensorC,2);
+    us_middleL = get_cms(usensormL,2);
+    us_middleR = get_cms(usensormR,2);
     
     
     if ( (us_center <=14) && (us_center > 0) ){
@@ -57,24 +57,25 @@ void mefRAupdate()
     
     move_stop();
     
-    us_right = get_cms(usensorR);
+    us_right = get_cms(usensorR,7);
     
-    if ( (us_right <=10) && (us_right > 0) ){
+    if ( (us_right <=14) && (us_right > 0) ){
       ra_State = turn_l;
       break;
     }else{
       ra_State = turn_r;
       break;
     }
+    
     delay(50);
     break;
 
   case turn_l:
       turn_left();
 
-      us_right = get_cms(usensorR);
-      us_center = get_cms(usensorC);
-      us_middleR = get_cms(usensormR);
+      us_right = get_cms(usensorR,2);
+      us_center = get_cms(usensorC,2);
+      us_middleR = get_cms(usensormR,2);
 
       //10
       if ( (us_right <=12) && (us_right > 0) && (us_center == 0 || us_center > 9 ) && (us_middleR == 0 || us_middleR > 7 ) ){
@@ -87,9 +88,9 @@ void mefRAupdate()
   case turn_r:
     turn_right();
     
-    us_left = get_cms(usensorL);
-    us_center = get_cms(usensorC);
-    us_middleL = get_cms(usensormL);
+    us_left = get_cms(usensorL,2);
+    us_center = get_cms(usensorC,2);
+    us_middleL = get_cms(usensormL,2);
    
     if ( (us_left <=12) && (us_left > 0) && (us_center == 0 || us_center > 9 ) && (us_middleL == 0 || us_middleL > 7 ) ){
       ra_State = init_;
